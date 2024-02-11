541478da6781985a4ac5a5c5103efaf27add055b
Declan <laresearchlabs@gmail.com>
2024-02-11 07:42:26 +0000

    draw stickman on canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log('testing')
+//used W3 Schools to work out how to draw the stickman
+const canvas = document.getElementById('stickmanCanvas');
+const draw = canvas.getContext("2d");
+
+// scaffold
+draw.beginPath()
+draw.moveTo(40, 230);
+draw.lineTo(10, 230)
+draw.lineTo(10, 10)
+draw.lineTo(150, 10)
+draw.lineTo(150, 25)
+draw.stroke();
+
+//Head
+draw.beginPath();
+draw.arc(150, 50, 25, 0, 2 * Math.PI);
+draw.stroke()
+//Body
+/*draw.beginPath();*/
+draw.moveTo(150, 75)
+draw.lineTo(150, 150)
+draw.stroke()
+//arms
+draw.moveTo(150, 100)
+draw.lineTo(100, 100)
+draw.stroke()
+draw.moveTo(150,100)
+draw.lineTo(200, 100)
+draw.stroke()
+//legs
+draw.moveTo(150,150)
+draw.lineTo(100, 200)
+draw.stroke()
+draw.moveTo(150, 150)
+draw.lineTo(200, 200)
+draw.stroke()
+
+
+
+console.log('testing again')
