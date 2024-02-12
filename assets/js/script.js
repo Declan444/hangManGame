331d4df2f00012ee03cf33453fa1b65e69edd806
Declan <laresearchlabs@gmail.com>
2024-02-12 20:24:13 +0000

    all body parts appear upon the revealButton click

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ const draw = canvas.getContext("2d");
 let bodyPart ={
     scaffold: false,
     head: false,
-    body: true,
-    leftArm: true,
-    rightArm: true,
-    leftLeg: true,
-    rightLeg: true
+    body: false,
+    leftArm: false,
+    rightArm: false,
+    leftLeg: false,
+    rightLeg: false
 
 }
 // function to draw the stickman in its parts
@@ -31,44 +31,62 @@ draw.beginPath();
 draw.arc(150, 50, 25, 0, 2 * Math.PI);
 draw.stroke()
 }
+
+
+
+//Body
+if(bodyPart.body){
+draw.moveTo(150, 75)
+draw.lineTo(150, 150)
+draw.stroke()
 }
+// left arm
+if(bodyPart.leftArm){
+draw.moveTo(150, 100)
+draw.lineTo(100, 100)
+draw.stroke()
+}
+if(bodyPart.rightArm){
+//right arm
+draw.moveTo(150,100)
+draw.lineTo(200, 100)
+draw.stroke()
+}
+if(bodyPart.leftLeg){
+//left leg
+draw.moveTo(150,150)
+draw.lineTo(100, 200)
+draw.stroke()
+}
+if(bodyPart.rightLeg){
+//right leg
+draw.moveTo(150, 150)
+draw.lineTo(200, 200)
+draw.stroke()
+}
+}
+
 function revealNextPart(){
     if(!bodyPart.scaffold){
         bodyPart.scaffold = true;
         console.log('calling this function')
     }else if (!bodyPart.head){
         bodyPart.head = true;
-        
+    }else if (!bodyPart.body){
+        bodyPart.body = true;
+    }else if (!bodyPart.leftArm){
+        bodyPart.leftArm = true;
+    }else if (!bodyPart.rightArm){
+        bodyPart.rightArm = true;
+    }else if (!bodyPart.leftLeg){
+        bodyPart.leftLeg = true;
+    }else if (!bodyPart.rightLeg){
+        bodyPart.rightLeg = true;
     }
+    
     drawStickman(bodyPart)
 }
 let revealButton = document.getElementById('revealButton')
 revealButton.addEventListener('click', revealNextPart)
-
-
-/*
-//Body
-
-draw.moveTo(150, 75)
-draw.lineTo(150, 150)
-draw.stroke()
-// left arm
-draw.moveTo(150, 100)
-draw.lineTo(100, 100)
-draw.stroke()
-//right arm
-draw.moveTo(150,100)
-draw.lineTo(200, 100)
-draw.stroke()
-//left leg
-draw.moveTo(150,150)
-draw.lineTo(100, 200)
-draw.stroke()
-//right leg
-draw.moveTo(150, 150)
-draw.lineTo(200, 200)
-draw.stroke()
-
-*/
 
 console.log('testing again')
